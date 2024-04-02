7985d5afdb9256bae2365dd50de6b56fd819f8db
CasarinDavide <casarindavide345@gmail.com>
2024-04-03 00:24:24 +0200

    - Implemented adjacency matrix inside graph class - commented with some possible code structure optimization

--- src/graphPackage/Graph.java
+++ src/graphPackage/Graph.java
@@ -1,12 +1,18 @@
 package graphPackage;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Graph<U extends Node,E extends Branches<U>> implements GraphInterface<U,E>{
 
+    // TODO togliamo le due liste e gestiamo tutto con la matrice di adiacenza?
     protected List<U> _nodes;
     protected List<E> _branches;
+    protected AdjacencyMatrix<U,E> _adjacencyMatrix;
+
+    // farei matrice di adiancenza posizionale non per indice ma per reference dell istanza.
+    // ad esempio:
+    // A[nodeI][nodeJ] nove nodeI e nodeJ sono instanze della classe graph.
+    // questo per accedere comunque in O(1)
+    // se facciamo matrice di adiacenza a questo punto non so quanto senso abbia avere all'interno della classe anche gli archi.
 
     @Override
     public void addNode(U node) {
